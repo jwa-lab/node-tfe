ae7f87e2432e5021bb43bcc20f2e2715d71894f9
Davide Talesco (IS-IE) <davide_talesco@trendmicro.com>
2020-12-21 12:16:01 +0000

    exported enums

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 import { Client } from './tfe';
+export { ApplyStatus } from './enums/ApplyStatus';
+export { PlanStatus } from './enums/PlanStatus';
+export { RunStatus } from './enums/RunStatus';
+export { WorkspaceVariableCategory } from './enums/WorkspaceVariableCategory';
 
 export default Client;
