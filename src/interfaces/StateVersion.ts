39fda67df822e7551e3e28fdc238f8297bf6a82b
Davide Talesco (IS-IE) <davide_talesco@trendmicro.com>
2021-01-06 08:05:02 +0000

    fixed state version interface

--- src/interfaces/StateVersion.ts
+++ src/interfaces/StateVersion.ts
@@ -6,11 +6,17 @@ import { StateVersionOutput } from './StateVersionOutput';
 export interface StateVersion {
   id: string; //  `jsonapi:"primary,state-versions"`
   createdAt: Date; //  `jsonapi:"attr,created-at,iso8601"`
-  downloadURL: string; //  `jsonapi:"attr,hosted-state-download-url"`
+  hostedStateDownloadUrl: string; //  `jsonapi:"attr,hosted-state-download-url"`
   serial: number; //   `jsonapi:"attr,serial"`
   vcsCommitSHA: string; //  `jsonapi:"attr,vcs-commit-sha"`
   vcsCommitURL: string; //  `jsonapi:"attr,vcs-commit-url"`
-
+  resources?: {
+    count: number;
+    module: string;
+    nam: string;
+    provider: string;
+    type: string;
+  }[];
   // Relations
   run: Run; //        `jsonapi:"relation,run"`
   outputs: StateVersionOutput[]; // `jsonapi:"relation,outputs"`
